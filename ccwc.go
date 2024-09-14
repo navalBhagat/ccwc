e5850630904d21b833d05b6d1226aaa8f43dca19
Naval Tapan Bhagat <59922532+navalBhagat@users.noreply.github.com>
2024-09-14 16:39:18 +0200

    Refactor

--- ccwc.go
+++ ccwc.go
@@ -6,15 +6,29 @@ import (
 )
 
 func main() {
-	fmt.Println(Count("test.txt"), "test.txt")
+	args := os.Args[1:]
+	flag := args[0]
+	filename := args[1]
+	switch flag {
+	case "-c":
+		fmt.Println(Count(filename)[0], "test.txt")
+	case "-l":
+		fmt.Println(Count(filename)[1], "test.txt")
+	case "-w":
+		fmt.Println(Count(filename)[2], "test.txt")
+	case "-m":
+		fmt.Println(Count(filename)[3], "test.txt")
+	default:
+		fmt.Println(Count(flag), "test.txt")
+	}
 }
 
-func Count(filename string) int {
+func Count(filename string) [4]int {
 	file, err := os.ReadFile(filename)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 	}
 
 	characterCount := len(file)
-	return characterCount
+	return [4]int{characterCount, 0, 0, 0}
 }
--- ccwc_test.go
+++ ccwc_test.go
@@ -8,7 +8,7 @@ func TestCharacterCount(t *testing.T) {
 	filename := "test.txt"
 	expected := 342190
 	result := Count(filename)
-	if result != expected {
-		t.Errorf("Count(%s) = %d; want %d", filename, result, expected)
+	if result[0] != expected {
+		t.Errorf("Expected characters in %s to be %d but was %d", filename, expected, result[0])
 	}
 }
